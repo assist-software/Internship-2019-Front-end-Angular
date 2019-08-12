7d3dde8d7ebbc94780505b67abd4acedecf4103f
Mihaela33 <mihaela.serediuc33@assist.ro>
2019-08-12 17:44:07 +0300

    Final de zi Luni, arhitectura componente adaugate

--- MovieAug/src/app/app.module.ts
+++ MovieAug/src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieListComponent } from './movies/movie-list/movie-list.component';
+import { MovieItemComponent } from './movies/movie-list/movie-item/movie-item.component';
+import { FooterComponent } from './footer/footer.component';
+import { MovieMainComponent } from './movies/movie-main/movie-main.component';
+import { MovieDetailComponent } from './movies/movie-detail/movie-detail.component';
+import { WatchlistComponent } from './watchlist/watchlist.component';
+import { LoginComponent } from './login/login.component';
+import { ResetComponent } from './reset/reset.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MoviesComponent,
+    MovieListComponent,
+    MovieItemComponent,
+    FooterComponent,
+    MovieMainComponent,
+    MovieDetailComponent,
+    WatchlistComponent,
+    LoginComponent,
+    ResetComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
