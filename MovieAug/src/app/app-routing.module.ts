7f08ed0c28205d485d9c4a5bd9b4f0e4262ec9b3
Mihaela33 <mihaela.serediuc33@assist.ro>
2019-08-13 15:29:32 +0300

    Add movie component

--- MovieAug/src/app/app-routing.module.ts
+++ MovieAug/src/app/app-routing.module.ts
@@ -5,14 +5,15 @@ import { RegisterComponent } from './register/register.component';
 import { ResetComponent } from './reset/reset.component';
 import { WatchlistComponent } from './watchlist/watchlist.component';
 import { HomeComponent } from './home/home.component';
-
+import { AddMovieComponent } from './administrator/add-movie/add-movie.component';
 
 const routes: Routes = [
-  {path: '', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: 'reset-password',component:ResetComponent},
-  {path: 'home',component:HomeComponent},
-  {path: 'watchlist',component: WatchlistComponent}
+  { path: '', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'reset-password', component: ResetComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'watchlist', component: WatchlistComponent },
+  { path: 'addMovie', component: AddMovieComponent }
 ];
 
 @NgModule({
