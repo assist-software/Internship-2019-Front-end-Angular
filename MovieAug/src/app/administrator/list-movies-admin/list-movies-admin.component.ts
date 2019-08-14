58fb07bf73f6342e582ec2093efea21c8eddcc9a
Mihaela33 <mihaela.serediuc33@assist.ro>
2019-08-14 17:37:16 +0300

    UI first page & add movie

--- MovieAug/src/app/administrator/list-movies-admin/list-movies-admin.component.ts
+++ MovieAug/src/app/administrator/list-movies-admin/list-movies-admin.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-list-movies-admin',
   templateUrl: './list-movies-admin.component.html',
   styleUrls: ['./list-movies-admin.component.css']
 })
+
 export class ListMoviesAdminComponent implements OnInit {
 
-  constructor() { }
+  movies: any[] = [
+    { name: "Dr Nice" },
+    { name: "bla bla" },
+    { name: "Dr Nice" },
+    { name: "Dr Nice" },
+    { name: "Dr Nice" },
+    { name: "Dr Nice" },
+  ];
+  constructor(private route: ActivatedRoute,
+    private router: Router, ) { }
 
   ngOnInit() {
   }
-
+  newMovie() {
+    console.log("Aici");
+    //this.router.navigate(['/app-add-movie']);
+  }
 }
