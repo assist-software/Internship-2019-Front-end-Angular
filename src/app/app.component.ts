78ab57628623d188d18d862d4e512aa702deb06b
denisPetriceanu2 <intern.denis.petriceanu@assist.ro>
2019-08-19 17:20:37 +0300

    Last form of aplication before create service for authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'WatchNext';
+export class AppComponent implements OnInit {
+  condition = false;
+  public href;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.href = this.router.url;
+    console.log('acest link: ' + this.router.url);
+  }
 }
