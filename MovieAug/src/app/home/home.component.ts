8ba3df021a6764234c946163dd11e815b573ec3e
razvan8888 <mircea.palie@assist.ro>
2019-08-22 14:56:45 +0300

    Update Watchlist and home- filter by date, name,IMBDScore

--- MovieAug/src/app/home/home.component.ts
+++ MovieAug/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RestApiService } from '../shared/rest-api.service';
 import { OrderPipe } from 'ngx-order-pipe';
 import { MoviesComponent } from '@app/movies/movies.component';
+import { movie } from '@app/shared/movie';
 
 @Component({
   selector: "app-home",
@@ -15,6 +16,7 @@ export class HomeComponent implements OnInit {
   index = 0;
   movieItem: any = [];
   movies: any = [];
+  nameDrop = 'Sort';
 
   constructor(
     public restApi: RestApiService,
@@ -28,12 +30,6 @@ export class HomeComponent implements OnInit {
     this.loadMovie();
   }
 
-  languages = ['Adventure', 'Fantasy', 'Action', 'Horror'];
-  // your $or filter
-  filter = { language: { $or: ['Adventure', 'Fantasy', 'Horror'] } };
-
-
-
   loadMovie() {
 
     return this.restApi.getMovies().subscribe((data: {}) => {
@@ -42,11 +38,53 @@ export class HomeComponent implements OnInit {
         this.movieItem = movie;
 
 
+
       }
 
       // console.log("Json");
       // console.log(data);
     })
+  }
+  sendFilter(sort: string) {
+    if (sort === 'titlu') {
+      this.movies.sort(this.sortFilterTitle)
+      this.nameDrop = 'Title'
+    } else if (sort === 'data') {
+      this.movies.sort(this.sortFilterData)
+      this.nameDrop = 'Data'
+    } else if (sort === 'score') {
+      this.movies.sort(this.sortFilterScore)
+      this.nameDrop = 'IMBD Score'
+    } else {
+      this.nameDrop = sort;
+      let filtered = this.movies.filter(movie => {
+        if (movie.Category === sort) {
+          return movie
+        }
+
+      });
+
+      console.log(filtered)
+
+
+    }
+  }
+
+  sortFilterTitle(c1: movie, c2: movie) {
+    if (c1.title > c2.title) return 1
+    else if (c1.title === c2.title) return 0
+    else return -1;
+  }
+  sortFilterData(c1: movie, c2: movie) {
+    if (c1.ReleaseDate > c2.ReleaseDate) return 1
+    else if (c1.ReleaseDate === c2.ReleaseDate) return 0
+    else return -1;
+  }
+  sortFilterScore(c1: movie, c2: movie) {
+    if (c1.IMDBScore > c2.IMDBScore) return -1
+    else if (c1.IMDBScore === c2.IMDBScore) return 0
+    else return 1;
+
   }
 }
 
--- MovieAug/src/app/watchlist/watchlist.component.ts
+++ MovieAug/src/app/watchlist/watchlist.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RestApiService } from '../shared/rest-api.service';
 import { OrderPipe } from 'ngx-order-pipe';
 import { MoviesComponent } from '@app/movies/movies.component';
+import { movie } from '@app/shared/movie';
 @Component({
   selector: 'app-watchlist',
   templateUrl: './watchlist.component.html',
@@ -10,13 +11,17 @@ import { MoviesComponent } from '@app/movies/movies.component';
 })
 export class WatchlistComponent implements OnInit {
   movieID: number;
+
   index = 0;
   movieItem: any = [];
   movies: any = [];
+  nameDrop = 'Sort';
+  filteredMoviesByGenre: any = []
 
   constructor(
     public restApi: RestApiService,
-    private orderPipe: OrderPipe,
+    private orderPipe: OrderPipe
+
 
 
   ) { }
@@ -26,9 +31,9 @@ export class WatchlistComponent implements OnInit {
     this.loadMovie();
   }
 
-  languages = ['Adventure', 'Fantasy', 'Action', 'Horror'];
-  // your $or filter
-  filter = { language: { $or: ['Adventure', 'Fantasy', 'Horror'] } };
+
+
+
 
   deleteMovie(Id) {
     console.log("delete", Id);
@@ -43,6 +48,7 @@ export class WatchlistComponent implements OnInit {
 
     return this.restApi.getMoviesUser().subscribe((data: {}) => {
       this.movies = data;
+
       for (let movie of this.movies) {
         this.movieItem = movie;
 
@@ -53,6 +59,49 @@ export class WatchlistComponent implements OnInit {
       // console.log(data);
     })
   }
+
+  sendFilter(sort: string) {
+    if (sort === 'titlu') {
+      this.movies.sort(this.sortFilterTitle)
+      this.nameDrop = 'Title'
+    } else if (sort === 'data') {
+      this.movies.sort(this.sortFilterData)
+      this.nameDrop = 'Data'
+    } else if (sort === 'score') {
+      this.movies.sort(this.sortFilterScore)
+      this.nameDrop = 'IMBD Score'
+    } else {
+      this.nameDrop = sort;
+      let filtered = this.movies.filter(movie => {
+        if (movie.Category === sort) {
+          return movie
+        }
+
+      });
+
+      console.log(filtered)
+
+
+    }
+  }
+
+  sortFilterTitle(c1: movie, c2: movie) {
+    if (c1.title > c2.title) return 1
+    else if (c1.title === c2.title) return 0
+    else return -1;
+  }
+  sortFilterData(c1: movie, c2: movie) {
+    if (c1.ReleaseDate > c2.ReleaseDate) return 1
+    else if (c1.ReleaseDate === c2.ReleaseDate) return 0
+    else return -1;
+  }
+  sortFilterScore(c1: movie, c2: movie) {
+    if (c1.IMDBScore > c2.IMDBScore) return -1
+    else if (c1.IMDBScore === c2.IMDBScore) return 0
+    else return 1;
+
+  }
+
 }
 
 
