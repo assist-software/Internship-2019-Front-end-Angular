bde8456930e8f272f8a67d8b4e42feb224fb26ff
PadurariuLiviu-git <intern.liviu.padurariu@assist.ro>
2019-08-22 17:45:02 +0300

    Design change

--- src/app/homePage/carousel-moovie/carousel-moovie.component.ts
+++ src/app/homePage/carousel-moovie/carousel-moovie.component.ts
@@ -13,29 +13,117 @@ export class CarouselMoovieComponent {
   slidesChangeMessage = '';
 
   slides = [
-    { img: './../../assets/pictures/image110.png' },
-    { img: './../../assets/pictures/image111.png' },
-    { img: './../../assets/pictures/image112.png' },
-    { img: './../../assets/pictures/image113.png' },
-    { img: './../../assets/pictures/image110.png' },
-    { img: './../../assets/pictures/image111.png' },
-    { img: './../../assets/pictures/image112.png' },
-    { img: './../../assets/pictures/image113.png' }
+    {
+      image: 'assets/pictures/image110.png',
+      title: 'Sparta',
+      trailerUrl: 'www.google.ro',
+      source: 'www.filme-bune.ro',
+      coverUrl: 'ww.my-cover.com',
+      description: 'aceasta descriere este una foarte buna dar nu stiu despre ce e',
+      category: [{
+        id: 1,
+        name: 'western',
+      }],
+      score: 10,
+      date: '2018-08-15'
+    },
+    {
+      image: 'assets/pictures/image111.png',
+      title: 'Aristotel',
+      trailerUrl: 'some text v1',
+      source: 'some text v1',
+      coverUrl: 'some text v1',
+      description: 'some text v1',
+      category: [{
+        id: 1,
+        name: 'clasic',
+      }],
+      score: 9,
+      date: '2019-08-15'
+    },
+    {
+      image: 'assets/pictures/image112.png',
+      title: 'Spanzuratoarea',
+      trailerUrl: 'www.google.ro',
+      source: 'www.filme-bune.ro',
+      coverUrl: 'ww.my-cover.com',
+      description: 'aceasta descriere este una foarte buna dar nu stiu despre ce e',
+      category: [{
+        id: 1,
+        name: 'aventura',
+      }],
+      score: 10,
+      date: '2018-08-15'
+    },
+    {
+      image: 'assets/pictures/image113.png',
+      title: 'Miracolul',
+      trailerUrl: 'some text v1',
+      source: 'some text v1',
+      coverUrl: 'some text v1',
+      description: 'some text v1',
+      category: [{
+        id: 1,
+        name: 'comedy',
+      }],
+      score: 9,
+      date: '2018-08-15'
+    },
+    {
+      image: 'assets/pictures/image110.png',
+      title: 'Bunastarea',
+      trailerUrl: 'www.google.ro',
+      source: 'www.filme-bune.ro',
+      coverUrl: 'ww.my-cover.com',
+      description: 'aceasta descriere este una foarte buna dar nu stiu despre ce e',
+      category: [{
+        id: 1,
+        name: 'drama',
+      }],
+      score: 10,
+      date: '2018-08-15'
+    },
+    {
+      image: 'assets/pictures/image111.png',
+      title: 'Visul',
+      trailerUrl: 'some text v1',
+      source: 'some text v1',
+      coverUrl: 'some text v1',
+      description: 'some text v1',
+      category: [{
+        id: 1,
+        name: 'triler',
+      }],
+      score: 9,
+      date: '2018-08-14'
+    }
   ];
 
   slideConfig = {
     slidesToShow: 4, slidesToScroll: 4, arrows: false,
     centerPadding: 'auto', centerMarging: '0px', centerMode: true, adaptiveHeight: true,
   };
-
+  totalslides = this.slides.length;
   constructor() { }
 
 
-
   addSlide() {
-    this.slides.push({ img: 'http://placehold.it/350x150/777777' });
+    this.slides.push({
+      image: 'assets/pictures/image110.png',
+      title: 'Bunastarea',
+      trailerUrl: 'www.google.ro',
+      source: 'www.filme-bune.ro',
+      coverUrl: 'ww.my-cover.com',
+      description: 'aceasta descriere este una foarte buna dar nu stiu despre ce e',
+      category: [{
+        id: 1,
+        name: 'drama',
+      }],
+      score: 10,
+      date: '2018-08-15'
+    });
   }
-  
+
 
   removeSlide() {
     this.slides.length = this.slides.length - 1;
--- src/app/homePage/higlight-moovie/higlight-moovie.component.ts
+++ src/app/homePage/higlight-moovie/higlight-moovie.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HiglightMoovieComponent implements OnInit {
 
+  moviehi = {
+    image: 'assets/pictures/image 4.2.png',
+    title: 'Moonlight',
+    trailerUrl: 'www.google.ro',
+    source: 'www.filme-bune.ro',
+    coverUrl: 'ww.my-cover.com',
+    description: 'A chronicle of the childhood, adolescence and burgeoning adulthood of a young, African-American, gay man growing up in a rough neighborhood of Miami.',
+    category: [{
+      id: 1,
+      name: 'western',
+    }],
+    score: 10,
+    date: 12321
+  };
+
   constructor() { }
 
   ngOnInit() {
